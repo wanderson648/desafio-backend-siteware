a289156bc39528813461246f5d994b15a74691d8
wanderson648 <testepedroca@gmail.com>
2024-01-08 23:05:20 -0300

    Cofiguraçcao inicial

---
+++ src/test/java/com/wo/siteware/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.siteware.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
